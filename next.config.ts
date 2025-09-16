ac369bd4d1ddef6210657aac9c5520415a9737de
hajracoder <hajraqadir94@gmail.com>
2025-09-16 09:51:18 -0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// // };
+
+// // export default nextConfig;
+
+
+
+
+
+
+
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.cdn2.seaart.me",
+        port: "",
+        pathname: "/2025-04-05/cvoa4qle878c73cu8gt0/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+
